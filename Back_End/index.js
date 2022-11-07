01c8a2021dcd4c635333838fe1ba1c5c14b27add
RollyLif <relifungula@gmail.com>
2022-11-07 15:59:48 +0200

    features: connecting to mongodb database on mongodb cloud

--- Back_End/index.js
+++ Back_End/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 2015;
+
+mongoose.connect('mongodb+srv://Rolly:LifeChat@cluster0.5kdlto6.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -8,6 +15,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+app.use(express.json());
 
 app.use('/', (req, res, next) => {
     const stuff = [
