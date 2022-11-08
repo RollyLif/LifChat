4f32080dcd831a9e1e121832d778070f4d9790b7
RollyLif <relifungula@gmail.com>
2022-11-08 10:53:47 +0200

    features : adding essential methods CRUD for our messages

--- Back_End/index.js
+++ Back_End/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Person = require('./Models/Person');
+const Message = require('./Models/Message');
 const app = express();
 const PORT = 2015;
 
@@ -24,18 +25,40 @@ app.use(express.urlencoded({extended:false}));
 app.post('/user', (req, res, next) =>{
     delete req.body._id;
     const person = new Person({
-        name : req.body.name,
-        password : req.body.password
+        ...req.body
     })
     person.save()
     .then(()=> res.status(201).json({message :'Objet enregistre'}))
     .catch(error => res.status(400).json({error}))
 })
 
-app.get('/user', (req, res, next) => {
+app.get('/user/:id', (req, res, next) =>{
+  Person.findOne({_id: req.params.id})
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(400).json({error}))
+})
+
+app.get('/users', (req, res, next) => {
     Person.find()
     .then(persons => res.status(200).json(persons))
     .catch(error => res.status(400).json({error}))
-  });
+});
+
+app.post('/message', (req, res, next)=>{
+  delete req.body._id;
+    const messenger = new Message({
+        ...req.body
+    })
+    messenger.save()
+    .then(()=> res.status(201).json({message :'Objet enregistre'}))
+    .catch(error => res.status(400).json({error}))
+});
+
+app.get('/messages/:id', (req, res, next) => {
+  Message.find({$or: [{'idSender':req.params.id},{'idReceiver':req.params.id}]})
+  .then(messenger => res.status(200).json(messenger))
+  .catch(error => res.status(400).json({error}))
+});
+
 
 app.listen(PORT, ()=>console.log("http://localhost:"+PORT));
