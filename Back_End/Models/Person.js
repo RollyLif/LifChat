241460c1adca49d3b95d0795d33b91f623846620
RollyLif <relifungula@gmail.com>
2022-11-08 12:16:15 +0200

    features : adding unique validator

--- Back_End/Models/Person.js
+++ Back_End/Models/Person.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const PersonSchema = mongoose.Schema({
-    name : { type : String, required : true},
+    name : { type : String, required : true, unique : true},
+    email : { type : String, required : true, unique : true},
     password : {type : String, required : true}
 });
+
+PersonSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Person', PersonSchema);
