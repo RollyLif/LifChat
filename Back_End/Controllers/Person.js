f2acc9bb07d6ed56a75652b44e2f82e220d3243c
RollyLif <relifungula@gmail.com>
2022-11-08 15:10:14 +0200

    features : adding login method

--- Back_End/Controllers/Person.js
+++ Back_End/Controllers/Person.js
@@ -17,6 +17,29 @@ exports.createPerson = (req, res, next) =>{
         .catch(error => res.status(500).json({error}));
 };
 
+exports.PersonLogin = (req, res, next) =>{
+    Person.findOne({$or: [{'name':req.body.username},{'email':req.body.username}]})
+      .then(user => {
+        if(user === null){
+            res.status(401).json({message : 'Paire identifiant/Mot de passe incorrect'})
+        } else {
+            bcrypt.compare(req.body.password, user.password)
+             .then(valid => {
+                if(!valid){
+                    res.status(401).json({message : 'Paire identifiant/Mot de passe incorrect'})
+                }else{
+                    res.status(200).json({
+                        userId : user._id,
+                        token : 'TOKEN'
+                    });
+                }
+             })
+             .catch(error => res.status(500).json({error}))
+        }
+      })
+      .catch(error => res.status(500).json({error}))
+}
+
 exports.ReadOnePerson = (req, res, next) =>{
     Person.findOne({_id: req.params.id})
       .then(thing => res.status(200).json(thing))
