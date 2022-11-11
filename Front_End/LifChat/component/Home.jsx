80bc9597b3b3cc3ee5f5b548969ce2b28ae9ae1d
RollyLif <relifungula@gmail.com>
2022-11-11 10:02:16 +0200

    features : validation client side

--- Front_End/LifChat/component/Home.jsx
+++ Front_End/LifChat/component/Home.jsx
@@ -14,6 +14,7 @@ function home() {
 
         <div className='col vw-50 my-auto'>
           <div className='card shadow' style={{width:'25rem'}}>
+            <p className='text-danger'>Paire Identifiant/Mot de passe incorrect</p>
             <input type="text" placeholder="Email ou nom d'utilisateur" className="form-control"/>
             <input type="password" placeholder="Mot de passe" className="form-control mt-2"/>
             <button className="btn btn-primary mt-3" onClick={()=>navigate("/Chat")}>Se connecter</button>
--- Front_End/LifChat/component/Register.jsx
+++ Front_End/LifChat/component/Register.jsx
@@ -1,6 +1,36 @@
-import React from 'react';
+import React,{ useState }  from 'react';
+import {validEmail, validName,validPWD} from '../models/Regex.js'
 
 function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [mdp, setMdp] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [emailErr, setEmailErr] = useState(false);
+  const [passwordErr, setPasswordErr] = useState(false);
+
+  const validate = () => {
+    if (validName.test(name)){
+      console.log('nom valid');
+    }
+    if (validEmail.test(email)) {
+       setEmailErr(true);
+       console.log('email avec succès');
+    }
+    if(validPWD.test(mdp)){
+      if(mdp === pwd){
+        setPasswordErr(true);
+        console.log('Mot de passe avec succès');
+      }
+    }else{
+      console.log('Mot de passe trop court');
+    }
+    
+
+    if(emailErr && passwordErr){
+      console.log('avec succès');
+    }
+ };
 
 return (
   <div className="App">
@@ -11,12 +41,16 @@ return (
 
       <div className='col vw-50 my-auto'>
         <div className='card shadow' style={{width:'25rem'}}>
-          <input type="text" placeholder="Nom d'utilisateur" className="form-control mt-2"/>
-          <input type="email" placeholder="Email" className="form-control mt-2"/>
-          <input type="password" placeholder="Mot de passe" className="form-control mt-2"/>
-          <input type="password" placeholder="Confirmez le mot de passe" className="form-control mt-2"/>
+          <input type="text" placeholder="Nom d'utilisateur" className="form-control mt-2" value={name}
+            onChange={(e) => setName(e.target.value)}/>
+          <input type="email" placeholder="Email" className="form-control mt-2" value={email}
+            onChange={(e) => setEmail(e.target.value)}/>
+          <input type="password" placeholder="Mot de passe" className="form-control mt-2" value={mdp}
+            onChange={(e) => setMdp(e.target.value)}/>
+          <input type="password" placeholder="Confirmez le mot de passe" className="form-control mt-2" value={pwd}
+            onChange={(e) => setPwd(e.target.value)}/>
           <div className='card-footer mt-3' style={{background :'white'}}>
-           <button className="btn btn-info">S'enregistrer</button>
+           <button className="btn btn-info" onClick={validate}>S'enregistrer</button>
           </div>
           <p>Retour à la <a href="./">page de connexion</a></p>
         </div>
