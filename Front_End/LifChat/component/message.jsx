ddeaadb5acc094172f1185129257e822984a8227
RollyLif <relifungula@gmail.com>
2022-12-05 14:59:21 +0200

    fix : authentification problem

--- Front_End/LifChat/component/message.jsx
+++ Front_End/LifChat/component/message.jsx
@@ -6,6 +6,7 @@ import { TbSend } from "react-icons/tb";
 import Dialogue from "./Dialogue";
 import MessageOut from "./MessageOut";
 import MessageIn from "./MessageIn";
+import Home from "./Home";
 import { v4 as uuidv4} from "uuid"
 import socketIO from 'socket.io-client';
 
@@ -98,7 +99,8 @@ function Message() {
 
   return (
     <div>
-      <section className="bg-primary">
+      {
+        (localStorage.getItem("log") == "true" ? <section className="bg-primary">
         <div className="text-white d-flex justify-content-between mx-2">
           <div className="d-flex align-items-center">
             <img
@@ -239,7 +241,8 @@ function Message() {
             </div>
           </div>
         </div>
-      </section>
+      </section>:<Home/>)
+      }
     </div>
   );
 }
