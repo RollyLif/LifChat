181d8fa1d0fb5eacba665c8b22bb702772e9a9ec
RollyLif <relifungula@gmail.com>
2022-12-07 16:06:22 +0200

    style: refactoring design of window discussion

---
+++ Front_End/LifChat/component/Discussion.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+
+const Discussion = () => {
+    return (
+        <div>
+            <div className="shadow rounded min-vw-75 p-3">
+                <Sidebar/>
+            </div>
+        </div>
+    );
+}
+
+export default Discussion;
--- Front_End/LifChat/src/App.jsx
+++ Front_End/LifChat/src/App.jsx
@@ -1,5 +1,5 @@
 import Home from '../component/Home'
-import './App.css'
+//import './App.css'
 
 function App() {
 
