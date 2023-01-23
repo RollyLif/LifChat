ed7e33d24de20a3f0a99b05efd8b030723a2e576
RollyLif <relifungula@gmail.com>
2023-01-23 09:21:22 +0200

    fixing problem with exportation of regexp

--- Back_End/Models/Regex.js
+++ Back_End/Models/Regex.js
@@ -1,3 +1,3 @@
-export const validEmail = new RegExp('^[a-zA-Z]([a-zA-Z0-9._-]{2,})@([a-z0-9._-]{3,})\.([a-z]{2,})$');
-export const validName = new RegExp("^\\w{2,}$");
-export const validPWD = new RegExp("^\\w{5,}$");
+exports.validEmail = new RegExp('^[a-zA-Z]([a-zA-Z0-9._-]{2,})@([a-z0-9._-]{3,})\.([a-z]{2,})$');
+exports.validName = new RegExp("^\\w{2,}$");
+exports.validPWD = new RegExp("^\\w{5,}$");
